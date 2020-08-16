e9e7fc268b1f2b8c1ee04a34a5555792311a757e
jgaunt96 <66772604+jgaunt96@users.noreply.github.com>
2020-08-16 10:07:08 +0100

    add delete product and general styling updates

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const { check, validationResult } = require('express-validator');
+
+const { handleErrors } = require('./middlewares');
 const usersRepo = require('../../repositories/users');
 const signupTemplate = require('../../views/admin/auth/signup');
 const signinTemplate = require('../../views/admin/auth/signin');
@@ -13,14 +14,12 @@ router.get('/signup', (req, res) => {
 
 
 
-router.post('/signup', [requireEmail, requirePassword, requirePasswordConfirmation], async (req, res) => {
+router.post('/signup', [requireEmail, requirePassword, requirePasswordConfirmation],
+    handleErrors(signupTemplate),
+    async (req, res) => {
 
-    const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        return res.send(signupTemplate({ req, errors}));
-    };
 
-    const {email, password, passwordConfirmation} = req.body;
+    const {email, password} = req.body;
     //create user in user repo
     const user = await usersRepo.create({email, password});
     //store the id of that user inside the users cookie
@@ -29,7 +28,7 @@ router.post('/signup', [requireEmail, requirePassword, requirePasswordConfirmati
 
     req.session.userId = user.id
 
-    res.send('Account created!');
+    res.redirect('/admin/products');
 });
 
 router.get('/signout', (req, res) => {
@@ -41,12 +40,9 @@ router.get('/signin', (req, res) => {
     res.send(signinTemplate({}));
 });
 
-router.post('/signin', [requireEmailExists, requireValidPasswordForUser], async (req, res) => {
-    const errors = validationResult(req);
-    
-    if(!errors.isEmpty()) {
-        return res.send(signinTemplate({ errors }));
-    }
+router.post('/signin', [requireEmailExists, requireValidPasswordForUser],
+    handleErrors(signinTemplate), 
+    async (req, res) => {
 
     const { email } = req.body;
 
@@ -54,7 +50,7 @@ router.post('/signin', [requireEmailExists, requireValidPasswordForUser], async 
     
     req.session.userId = user.id;
 
-    res.send('You are signed in');
+    res.redirect('/admin/products');
 });
 
 module.exports = router;
